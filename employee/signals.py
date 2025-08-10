374a628cd903009c71e9ca5e889d8b34aa5bcde7
yego21 <jakevillamor222@gmail.com>
2025-08-10 19:27:05 +0800

    fix signal

--- employee/signals.py
+++ employee/signals.py
@@ -7,16 +7,16 @@ from company.models import Department
 import sys
 
 @receiver(post_save, sender=User)
+
 def manage_user_profile(sender, instance, created, **kwargs):
-    def manage_user_profile(sender, instance, created, **kwargs):
-        print(f'Creating userprofile for User: {instance}', file=sys.stdout, flush=True)
-        if created:
-            # Create UserProfile if it doesn't already exist
-            UserProfile.objects.get_or_create(user=instance)
-        else:
-            # Ensure UserProfile is saved if it already exists
-            if hasattr(instance, 'userprofile'):
-                instance.userprofile.save()
+    print(f'Creating userprofile for User: {instance}', file=sys.stdout, flush=True)
+    if created:
+        # Create UserProfile if it doesn't already exist
+        UserProfile.objects.get_or_create(user=instance)
+    else:
+        # Ensure UserProfile is saved if it already exists
+        if hasattr(instance, 'userprofile'):
+            instance.userprofile.save()
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
